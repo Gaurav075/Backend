6ddc1f8922caaef7ce1b323d8c7e9ac65b6baa82
Gaurav075 <125338113+Gaurav075@users.noreply.github.com>
2024-12-18 09:30:37 +0000

    multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+
+const uploadoncloudinary = async(localfilepath)=>{
+    try {
+        if (!localfilepath) {
+            return null;
+        }
+        // upload file
+        const response = await cloudinary.uploader.upload(localfilepath,{
+            resource_type:"auto"
+        })
+        // file has been uploaded successfully
+        console.log("file is uploaded on cloudinary",response.url);
+        return response
+        
+    } catch (error) {
+        fs.unlinkSync(localfilepath) // remove the locally saved temporray file as the upload operation fot failed
+        return null;
+    }
+
+
+}
+
+export {uploadoncloudinary}
