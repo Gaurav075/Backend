e05cb48eb27cbce8a46231b353e5f9db06fbf034
Gaurav075 <125338113+Gaurav075@users.noreply.github.com>
2024-12-22 14:14:21 +0000

    login,logout  created

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,32 @@
+import {asyncHandler} from "../utils/asyncHandler"
+import jwt from "jsonwebtoken"
+import {Apierror} from "../utils/Apierror"
+import {User} from "../models/user.model"
+
+export const verifyJWT = asyncHandler(async(req,_,next)=>{
+    try {
+        const token = req.cookies?.accessToken || req.header
+        ("Authorisation")?.replace("Bearer ","")
+    
+        if(!token){
+            throw new Apierror(401,"Unauthorised request")
+        }
+    
+        const decodedToken = jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+        const user = await User.findById(decodedToken?._id)
+        .select("-password -refreshToken")
+    
+        if(!user)
+        {
+            // TODO: discuss about frontend next video
+            throw new Apierror(401,"Invalid Access Token")
+        }
+    
+        req.user = user
+        next()
+    } catch (error) {
+        throw new Apierror(401,error?.message || 
+            "Invalid access token"
+        )
+    }
+})
